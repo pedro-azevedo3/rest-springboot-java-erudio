0ba3d0716f48cc7efa487470b105232e5b77f071
Dev Pedro Azevedo <pedroazevedoteixeira7@gmail.com>
2024-02-02 19:45:26 -0300

    Added more mocks

--- rest-springboot-java-erudio/src/main/java/br/com/erudio/br/com/erudio/services/PersonServices.java
+++ rest-springboot-java-erudio/src/main/java/br/com/erudio/br/com/erudio/services/PersonServices.java
@@ -3,6 +3,8 @@ package br.com.erudio.br.com.erudio.services;
 import br.com.erudio.br.com.erudio.models.Person;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 import java.util.logging.Logger;
 
@@ -11,6 +13,18 @@ public class PersonServices {
     private final AtomicLong counter = new AtomicLong();
 
     private Logger logger = Logger.getLogger(PersonServices.class.getName());
+
+    public List<Person> findAll() {
+        logger.info("Finding all people");
+        List<Person> persons = new ArrayList<>();
+        for (int i = 0; i < 8; i++) {
+            Person person = mockPerson(i);
+            persons.add(person);
+        }
+        return persons;
+    }
+
+
 
     public Person findById(String id) {
         logger.info("Finding one person");
@@ -23,6 +37,16 @@ public class PersonServices {
         person.setLastName("Azevedo");
         person.setGender("Male");
 
+        return person;
+    }
+
+    private Person mockPerson(int i) {
+        Person person = new Person();
+        person.setId(counter.incrementAndGet());
+        person.setAddress("Some Address in Brazil " + i);
+        person.setFirstName("First Name " + i);
+        person.setLastName("Last Name " + i);
+        person.setGender("Gender");
         return person;
     }
 }
